da6c2fbaed5323b3f4aea82e83d1950962a0d29a
palonsovazquez <pav.vigo@gmail.com>
2019-03-10 17:50:31 +0100

    Commit boletin 16

---
+++ Boletin_16/src/boletin_16/Canario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_16;
+
+/**
+ *
+ * @author Pablo Alonso Vazquez <pav.vigo@gmail.com>
+ */
+public class Canario implements IPodeCantar{
+
+    @Override
+    public void cantar() {
+        System.out.println("Pio,Pio");    
+    }
+    
+}
